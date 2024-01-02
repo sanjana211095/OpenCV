80602b687f67fdf892c7f48ce5d8708d55296f32
Sanjana Bafana <sanjanabafna093@gmail.com>
2024-01-02 19:38:08 +0530

    done for today

---
+++ Section 1/read_images.py
@@ -0,0 +1,5 @@
+import cv2
+image = cv2.imread("Images/shapes.png")
+cv2.imshow("Output",image)
+cv2.waitKey(0)
+
---
+++ Section 1/read_videos.py
@@ -0,0 +1,14 @@
+import cv2
+
+video = cv2.VideoCapture("Videos/2.mp4")
+while True:
+    success,frame = video.read()
+    if success:
+        cv2.imshow("Output",frame)
+        if cv2.waitKey(1) & 0xFF==ord('q'):
+            break
+    else:
+        break
+
+video.release()
+cv2.destroyAllWindows()
